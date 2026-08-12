97e96cde04d9d12280958ab554d3565227dbf554
taylorpapke <taylorpapke@gmail.com>
2026-08-11 20:38:22 -0700

    fixed tailwind bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import tailwindConfig from './tailwind.config.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
 
   vite: {
-    plugins: [tailwindcss({ config: tailwindConfig })]
+    plugins: [tailwindcss()]
   }
 });
