463efe9afebb1a5c14c0e1a51299657ff978fafb
taylorpapke <taylorpapke@gmail.com>
2026-09-21 10:37:24 -0700

    removed nested core values card on about us page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,22 +2,6 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1.5rem', 
-        md: '2rem',        
-        lg: '4rem',        
-        xl: '7.1875rem',   
-      },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1440px',   
-      },
-    },
     extend: {
       colors: {
         // NEW SEMANTIC DESIGN TOKENS
